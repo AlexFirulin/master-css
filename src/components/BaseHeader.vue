a30184a28bd0382d911e3e4dcc75b5ef0a47a19a
AlexFirulin <1993firulin@gmail.com>
2025-02-05 13:51:57 +0200

    header

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,7 +1,106 @@
+<script setup lang="ts">
+import BaseHeaderButton from './Buttons/BaseHeaderButton.vue';
+
+</script>
+
 <template>
-    <header class="header">
-        Some Text
-    </header>
+  <header class="header">
+    <div class="logo-box">
+      <img src="../assets/images/logo-white.png" alt="logo" class="logo" />
+    </div>
+    <div class="text-box">
+      <h1 class="heading-primary">
+        <span class="heading-primary-main">Outdoors</span>
+        <span class="heading-primary-sub">is where life happens</span>
+      </h1>
+      <BaseHeaderButton />
+    </div>
+  </header>
 </template>
 
-<style scoped></style>
+<style scoped>
+.header {
+  position: relative;
+  height: 95vh;
+  background-image: linear-gradient(to right bottom, #7ed56fc7, #28b485c4),
+    url(../assets/images/hero.jpg);
+  background-size: cover;
+  background-position: top;
+  clip-path: polygon(0 0, 100% 0, 100% 75vh, 0 100%);
+}
+
+.logo-box {
+  position: absolute;
+  top: 40px;
+  left: 40px;
+}
+
+.logo {
+  height: 35px;
+}
+
+.text-box {
+    position: absolute;
+    top: 40%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+}
+
+.heading-primary {
+  color: #fff;
+  text-transform: uppercase;
+  backface-visibility: hidden;
+  margin-bottom: 60px;
+}
+
+.heading-primary-main {
+  display: block;
+  font-size: 60px;
+  font-weight: 400;
+  letter-spacing: 35px;
+  animation-name: moveToLeft;
+  animation-duration: 1s;
+  animation-timing-function: ease-out;
+  /* animation-delay: 1s;
+  animation-iteration-count: 3; */
+}
+
+.heading-primary-sub {
+  display: block;
+  font-size: 20px;
+  font-weight: 400;
+  letter-spacing: 17px;
+  animation-name: moveToRight;
+  animation-duration: 1s;
+  animation-timing-function: ease-out;
+}
+
+@keyframes moveToLeft {
+    0% {
+        opacity: 0;
+        transform: translateX(-100px);
+    }
+    80% {
+        transform: translateX(10px);
+    }
+    100% {
+        opacity: 1;
+        transform: translate(0);
+    }
+}
+
+@keyframes moveToRight {
+    0% {
+        opacity: 0;
+        transform: translateX(100px);
+    }
+    80% {
+        transform: translateX(-10px);
+    }
+    100% {
+        opacity: 1;
+        transform: translate(0);
+    }
+}
+</style>
---
+++ src/components/Buttons/BaseHeaderButton.vue
@@ -0,0 +1,65 @@
+<template>
+    <a href="#" class="btn btn-white btn-animated">Discover our tools</a>
+</template>
+<style scoped>
+.btn:link,
+.btn:visited {
+    text-transform: uppercase;
+    text-decoration: none;
+    padding: 15px 40px;
+    display: inline-block;
+    border-radius: 100px;
+    transition: all .2;
+    position: relative;
+}
+
+.btn:hover {
+    transform: translateY(-3px);
+    box-shadow: 0 10px 20px rgba(0, 0, 0, .2);
+}
+
+.btn:active {
+    transform: translateY(-1px);
+    box-shadow: 0 5px 10px rgba(0, 0, 0, .2);
+}
+.btn-white {
+    background-color: #fff;
+    color: #777;
+}
+.btn::after {
+ content: '';
+ display: inline-block;
+ height: 100%;
+ width: 100%;
+ border-radius: 100px;
+ z-index: -1;
+ position: absolute;
+ top: 0;
+ left: 0;
+ transition: all .4s;
+}
+
+.btn-white::after {
+    background-color: #fff;
+}
+
+.btn:hover::after {
+    transform: scale(1.5);
+    opacity: 0;
+}
+@keyframes animate {
+    0% {
+        opacity: 0;
+        transform: translateY(30px);
+    }
+    100% {
+        opacity: 1;
+        transform: translate(0);
+    }   
+}
+
+.btn-animated {
+    animation: animate .5s ease-out .75s;
+    animation-fill-mode: backwards;
+}
+</style>
