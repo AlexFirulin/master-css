c4bcad9c50db9285e2f8c9a77baa32135ab96f88
AlexFirulin <1993firulin@gmail.com>
2025-02-11 14:15:34 +0200

    cols and rows

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
+import BaseInfo from './components/BaseInfo.vue';
 import Default from './layouts/default.vue';
 
 </script>
 
 <template>
-    <Default></Default>
+    <Default>
+        <main>
+            <BaseInfo />
+        </main>
+    </Default>
 </template>
 
 <style scoped>
---
+++ src/components/BaseInfo.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="section-about">
+    \
+    <div class="u-center-text u-margin-bottom-8">
+      <h2 class="heading-secondary">Excluding adventure of people</h2>
+    </div>
+    <div class="row">
+      <div class="col-1-2">Text COntent</div>
+      <div class="col-1-2">Image composition</div>
+    </div>
+  </section>
+</template>
+<style lang="scss" scoped>
+@use "./../assets/styles/abstracts/variables" as *;
+
+.section-about {
+  background-color: $color-grey-light-1;
+  padding: 25rem 0;
+  margin-top: -20vh;
+}
+</style>
