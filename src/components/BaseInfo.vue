e6f3248412a05252636ef195b104b329819d513c
AlexFirulin <1993firulin@gmail.com>
2025-02-12 16:11:40 +0200

    updates?

--- src/components/BaseInfo.vue
+++ src/components/BaseInfo.vue
@@ -1,12 +1,35 @@
 <template>
   <section class="section-about">
     \
-    <div class="u-center-text u-margin-bottom-8">
+    <div class="u-center-text u-margin-bottom-big">
       <h2 class="heading-secondary">Excluding adventure of people</h2>
     </div>
     <div class="row">
-      <div class="col-1-2">Text COntent</div>
-      <div class="col-1-2">Image composition</div>
+      <div class="col-1-2">
+        <h3 class="heading-tertiary u-margin-bottom-small">
+          You`re going to fall in love with nature
+        </h3>
+        <p class="paragraph">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint maxime
+          quaerat voluptate laudantium, impedit tempore eaque laboriosam quod,
+          nobis quas facere, obcaecati cumque dicta non ad praesentium
+          inventore. Recusandae, adipisci!
+        </p>
+
+        <h3 class="heading-tertiary u-margin-bottom-small">
+          Live adventure like you never live before
+        </h3>
+        <p class="paragraph">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint maxime
+          quaerat voluptate laudantium, impedit tempore eaque laboriosam quod,
+          
+        </p>
+
+        <a href="#" class="btn-text">Learn more &rarr;</a>
+      </div>
+      <div class="col-1-2">
+        You`re going to fall in love with nature
+      </div>
     </div>
   </section>
 </template>
